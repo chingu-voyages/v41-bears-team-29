95543870f323642839899f3d54c7f1e544e3af08
letsbehuman <letsbehumandesign@gmail.com>
2022-10-27 16:03:11 +0200

    adding newUserBtn

--- Front-end/components/newUserBtn.js
+++ Front-end/components/newUserBtn.js
@@ -1,19 +1,19 @@
 import { Feather } from "@expo/vector-icons";
 import { StyleSheet } from "react-native";
 
-export default function CameraBtn() {
-  return <Feather name="camera" style={styles.camera} />;
+export default function NewUserBtn() {
+  return <Feather name="user-plus" style={styles.camera} />;
 }
 const styles = StyleSheet.create({
   camera: {
-    fontSize: 90,
+    fontSize: 60,
     textAlignVertical: "center",
-    width: 150,
-    height: 150,
+    width: 70,
+    height: 70,
     borderWidth: 5,
     borderRadius: 100,
     textAlign: "center",
-    borderColor: "#BEAEE2",
+    borderColor: "#F3C583",
     backgroundColor: "#FEF1E6",
   },
 });
